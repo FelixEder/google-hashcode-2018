6f0f1fce2e17411ce0c670d36efcf2bd0ffd356c
felixed <felixed@kth.se>
2018-03-01 20:32:06 +0100

    Fixed distance

--- Car.java
+++ Car.java
@@ -1,8 +1,10 @@
 
 public class Car {
-  private int x, y, distance;
+  private int x, y;
+  private int distanceToStart, distanceToEnd;
   private Ride currentRide;
   private ArrayList<Ride> ridesDriven;
+  private boolean pickedUp;
 
   public Car(int x, int y) {
     this.x = x;
@@ -13,7 +15,8 @@ public class Car {
 
   public void assignRide(Ride ride){
     currentRide = ride;
-    distance = Math.abs(x - currentRide.getFinishRow()) + Math.abs(y - currentRide.getFinishColumn());
+    distanceToStart = Math.abs(x - currentRide.getStartRow()) + Math.abs(y - currentRide.getStartColumn());
+    distanceToEnd = Math.abs(x - currentRide.getFinishRow()) + Math.abs(y - currentRide.getFinishColumn());
   }
 
   public int getX(){
@@ -36,12 +39,23 @@ public class Car {
     return (currentRide == null);
   }
   public void drive(){
-    distance--;
-    if (distance == 0) {
-      ridesDriven.add(currentRide);
-      x = currentRide.getFinishRow();
-      y = currentRide.getFinishColumn();
-      currentRide = null;
+    if(pickedUp) {
+      distanceToEnd--;
+      if (distanceToEnd == 0) {
+        ridesDriven.add(currentRide);
+        x = currentRide.getFinishRow();
+        y = currentRide.getFinishColumn();
+        currentRide = null;
+      }
+    } else {
+      if(distanceToStart == 0) {
+        if(Main.time >= currentRide.getEarliestStart()) {
+          pickedUp = true;
+          drive();
+        }
+      } else {
+        distanceToStart--;
+      }
     }
   }
 }
