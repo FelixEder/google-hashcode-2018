04624bad5aad0f1d81a20bdad15c6b5196881f11
felixed <felixed@kth.se>
2018-03-01 18:21:57 +0100

    Added a main java file

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		input();
+		output();
+	}
+
+	public static void input() throws FileNotFoundException{
+		//Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(new File("/home/felix/Dokument/Projekt/google-hashcode-2017/kittens.in"));
+
+		//TODO Here scan things to input
+	}
+
+	public static void output() {
+		//TODO Here do some system out
+	}
+}
